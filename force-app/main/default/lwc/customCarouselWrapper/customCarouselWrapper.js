49feca00e273d1c65b5a339426dea2c2d48a1ced
Sreeja P M <sreeja.pm@enfintechnologies.com>
2024-01-09 17:30:01 +0530

    After completing components for Premium, Standard and Single Rooms

---
+++ force-app/main/default/lwc/customCarouselWrapper/customCarouselWrapper.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+import CarouselImages from '@salesforce/resourceUrl/HostelBookingImages';
+export default class CustomCarouselWrapper extends LightningElement {
+    slides = [{
+        image:`${CarouselImages}/CarouselImages/Premium001.jpg`,
+        heading:'Room 1',
+        desc:'Description part here'
+    },
+    {
+        image:`${CarouselImages}/CarouselImages/Premium002.jpg`,
+        heading:'Room 2',
+        desc:'Description part here'
+    },
+    {
+        image:`${CarouselImages}/CarouselImages/Premium003.jpg`,
+        heading:'Room 3',
+        desc:'Description part here'
+    }]
+}
